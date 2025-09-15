07bc6268d85d9688eaf5ed867711d49c60eba96d
Musakalamz <ogunsolumusa@gmail.com>
2025-09-15 18:01:30 +0100

    Styles optionally included in the interface

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -7,20 +7,23 @@ const landing: React.FC = () => {
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
 
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-4 gap-4">
         {/* Small Buttons */}
         <Button title="Small - sm" size="small" shape="rounded-sm" />
         <Button title="Small - md" size="small" shape="rounded-md" />
+        <Button title="Small - md" size="small" shape="rounded-lg" />
         <Button title="Small - full" size="small" shape="rounded-full" />
 
         {/* Medium Buttons */}
         <Button title="Medium - sm" size="medium" shape="rounded-sm" />
         <Button title="Medium - md" size="medium" shape="rounded-md" />
+        <Button title="Medium - md" size="medium" shape="rounded-lg" />
         <Button title="Medium - full" size="medium" shape="rounded-full" />
 
         {/* Large Buttons */}
         <Button title="Large - sm" size="large" shape="rounded-sm" />
         <Button title="Large - md" size="large" shape="rounded-md" />
+        <Button title="Large - md" size="large" shape="rounded-lg" />
         <Button title="Large - full" size="large" shape="rounded-full" />
       </div>
     </div>
